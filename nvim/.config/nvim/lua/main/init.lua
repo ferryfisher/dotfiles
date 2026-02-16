6fb2362ad0d9b3a29ab94bd36406bfa9c1253f02
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-02-15 20:53:34 -0800

    feat: Update nvim main

--- nvim/.config/nvim/lua/main/init.lua
+++ nvim/.config/nvim/lua/main/init.lua
@@ -1,6 +1,16 @@
 local autocmd = vim.api.nvim_create_autocmd
 local group = vim.api.nvim_create_augroup("ferry", {})
 
+autocmd("CmdlineEnter", {
+    group = group,
+    once = true,
+    callback = function()
+        if vim.version().minor < 12 then return end
+
+        require("vim._core.ui2").enable({})
+    end
+})
+
 autocmd("TextYankPost", {
     group = group,
     callback = function()
@@ -26,6 +36,8 @@ autocmd("UIEnter", {
             require("main.keymap")
             require("main.statusline")
             require("main.lsp")
+
+            vim.lsp.log.set_level(vim.log.levels.OFF)
 
             vim.cmd.packadd("nvim.undotree")
         end)
