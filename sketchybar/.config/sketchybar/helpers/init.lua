2ae1bf53944c8d4ea3525e2513bac56af1e60596
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-06-20 15:54:11 -0700

    refactor(sketchybar): remove .luarc.json and replace os.execute

--- sketchybar/.config/sketchybar/helpers/init.lua
+++ sketchybar/.config/sketchybar/helpers/init.lua
@@ -1,4 +1,5 @@
 -- Add the sketchybar module to the package cpath
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.config/sketchybar/sketchybar_lua/?.so"
 
-os.execute("(cd helpers && make)")
+sbar = require "sketchybar"
+sbar.exec("(cd helpers && make)")
--- sketchybar/.config/sketchybar/init.lua
+++ sketchybar/.config/sketchybar/init.lua
@@ -1,6 +1,3 @@
--- Require the sketchybar module
-sbar = require "sketchybar"
-
 -- Bundle the entire initial configuration into a single message to sketchybar
 sbar.begin_config()
 require "bar"
