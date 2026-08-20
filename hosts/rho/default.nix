2e88f461fdcbf55d0d7ed25b979023fbb849bfb9
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-08-20 00:43:22 -0700

    feat(hosts): add determinate-nix config

--- hosts/rho/default.nix
+++ hosts/rho/default.nix
@@ -1,4 +1,15 @@
 {
   # using determinate-nix
   nix.enable = false;
+
+  environment.etc."nix/nix.custom.conf".text = ''
+    # Managed declaratively.
+    experimental-features = auto-allocate-uids flakes nix-command
+
+    accept-flake-config = false
+    auto-allocate-uids = true
+    auto-optimise-store = true
+    use-xdg-base-directories = true
+    warn-dirty = false
+  '';
 }
