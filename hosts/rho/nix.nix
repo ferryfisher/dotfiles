bbca5c26227fed276bd69e71e3a80d7884a0987b
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-08-25 01:07:39 -0700

    feat: more nix settings

--- hosts/rho/nix.nix
+++ hosts/rho/nix.nix
@@ -7,8 +7,10 @@
     experimental-features = auto-allocate-uids flakes nix-command
 
     accept-flake-config = false
+    allow-import-from-derivation = false
     auto-allocate-uids = true
     auto-optimise-store = true
+    keep-going = true
     use-xdg-base-directories = true
     warn-dirty = false
   '';
