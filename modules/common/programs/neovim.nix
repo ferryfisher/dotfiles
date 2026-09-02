e4f44e683541c5e5e133f48ed3cfeddd75a3e545
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-09-02 00:36:09 -0700

    feat: add versioning to neovim pkg

--- modules/common/programs/neovim.nix
+++ modules/common/programs/neovim.nix
@@ -1,11 +1,27 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  self,
+  ...
+}:
 
+let
+  lock = builtins.fromJSON (builtins.readFile (self + "/.tack/pins.lock.json"));
+  rev = builtins.substring 0 12 lock.neovim.rev;
+in
 {
   environment.systemPackages = [
-    (pkgs.neovim-unwrapped.overrideAttrs {
-      doInstallCheck = false;
+    (pkgs.neovim-unwrapped.overrideAttrs (oldAttrs: {
       src = inputs.neovim;
-    })
+      version = rev;
+
+      postPatch = /* bash */ ''
+        ${oldAttrs.postPatch or ""}
+
+        substituteInPlace cmake.config/versiondef.h.in --replace-fail \
+        '@NVIM_VERSION_PRERELEASE@' '-nightly+${rev}'
+      '';
+    }))
   ];
 
   preferences = {
