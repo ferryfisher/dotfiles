6cb710dad8490cf39973dd1b6d31071a1e8fe5d9
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-09-06 06:13:35 -0700

    fix(hosts): disable telemetry

--- hosts/rho/nix.nix
+++ hosts/rho/nix.nix
@@ -2,15 +2,26 @@
   # using determinate-nix
   nix.enable = false;
 
-  environment.etc."nix/nix.custom.conf".text = ''
-    # Managed declaratively.
-    experimental-features = auto-allocate-uids flakes nix-command
+  environment = {
+    variables = {
+      DETSYS_IDS_TELEMETRY = "disabled";
+      NIX_SENTRY_ENDPOINT = "";
+    };
 
-    accept-flake-config = false
-    allow-import-from-derivation = false
-    auto-allocate-uids = true
-    keep-going = true
-    use-xdg-base-directories = true
-    warn-dirty = false
-  '';
+    etc."determinate/config.json".text = builtins.toJSON {
+      telemetry.sentry.endpoint = null;
+    };
+
+    etc."nix/nix.custom.conf".text = ''
+      # Managed declaratively.
+      experimental-features = auto-allocate-uids flakes nix-command
+
+      accept-flake-config = false
+      allow-import-from-derivation = false
+      auto-allocate-uids = true
+      keep-going = true
+      use-xdg-base-directories = true
+      warn-dirty = false
+    '';
+  };
 }
