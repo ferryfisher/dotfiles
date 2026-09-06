c7eb9e0fbeb052a8088890e0c49ccc7b9453fdef
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-09-06 06:13:05 -0700

    fix(system): fix paths to nix-gc

--- modules/darwin/system/nix-gc.nix
+++ modules/darwin/system/nix-gc.nix
@@ -3,16 +3,25 @@
 {
   launchd.daemons.nix-gc = {
     command = "${lib.meta.getExe pkgs.nh} clean all --keep 5 --optimise";
-    path = [ "/nix/var/nix/profiles/default/bin" ];
+
+    path = [
+      "/nix/var/nix/profiles/default/bin"
+      "/usr/bin"
+      "/bin"
+      "/usr/sbin"
+      "/sbin"
+    ];
 
     serviceConfig = {
       Label = "org.nix.nix-gc";
       StartCalendarInterval = {
-        Hour = 12;
+        Hour = 13;
         Minute = 0;
       };
-      StandardErrorPath = "/var/log/nix-gc.error.log";
-      StandardOutPath = "/var/log/nix-gc.out.log";
+      LowPriorityIO = true;
+      ProcessType = "Background";
+      StandardErrorPath = "/var/log/nix-gc.log";
+      StandardOutPath = "/var/log/nix-gc.log";
     };
   };
 }
