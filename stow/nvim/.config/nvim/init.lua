b17ae3546fe7ef0b8403245fc2d961475d486eef
Ferry <217118383+ferryfisher@users.noreply.github.com>
2026-09-07 03:21:22 -0700

    chore(nvim): disable more unused builtin plugins

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -40,11 +40,13 @@ require("lazy").setup("plugins", {
 
         rtp = {
             disabled_plugins = {
+                "dir",
                 "editorconfig",
                 "gzip",
                 "matchit",
                 "netrw",
                 "netrwPlugin",
+                "rplugin",
                 "tarPlugin",
                 "tutor",
                 "zip",
